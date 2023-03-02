51b5b0a2cc97445a3c6d32242080de3152d58541
Totrous <62214566+KiraMelonhead@users.noreply.github.com>
2023-03-02 08:08:04 +0300

    Update and rename num_pyramid(completed_r).py to do_Strings.py

--- do_Strings.py
+++ do_Strings.py
@@ -1,6 +1,7 @@
 # This project is all 'bout string operators.
 # An input text will be processed through chosen string method
 # The string method determined by the user's(me) input
+# It has to be here words looking statement(for "word" in)
 
 cte = '"insert text for conducting our testing:\n"'
 print(len(cte))
@@ -11,18 +12,30 @@ if request == "chop":
     ending = int(input())
     print(cte[start: ending])
 
-elif request == "upper":
-    print(cte.upper())
+elif request == "list":
+    lT = input("what type of list?\n")
+    if lT == "whole":
+        for w in cte:
+            print(w)
+    elif lT == "a slice":
+        a_s = int(input("the index of the slice?\n"))
+        print(cte[a_s])
+    elif lT == "cut 'til end":
+        print(cte[int(input()):])
+    elif lT == "cut end to 'til":
+        print(cte[:int(input())])
 
+elif request == "upper":   # upper and lower brothers are here
+    print(cte.upper())
 elif request == "lower":
     print(cte.lower())
 
-elif request == "replace":
+elif request == "replace":    # replace method is on position
     t_object = input("replacing object: ")
     p_object = input("given object: ")
     print(cte.replace(t_object, p_object))
 
-elif request == "check":
+elif request == "check":                # FKN helper erased my comment
     check = input("look for:\n")
     print(check in cte)
 
@@ -36,29 +49,11 @@ elif request == "check&count":
         print(count)
     else:
         print("There is no such a character")
-# It's fiiiiiiiiiiiiiiiiiiiiiiiiine
-# Why is it "fine" though? What is even wrong here?
-
-# I don't know why I wrote this comment here nut for now the problem is that I cannot print count properly
-# without spamming the screen with "There is no such a character"
 
 
 elif request == "print":
     print(cte)
 
-elif request == "list":
-    lT = input("what type of list?\n")
-    if lT == "whole":
-        for w in cte:
-            print(w)
-    elif lT == "a slice":
-        a_s = int(input("the index of the slice?\n"))
-        print(cte[a_s])
-    elif lT == "cut 'til end":
-        print(cte[int(input()):])
-# Here I just got bored and put all in one line
-    elif lT == "cut end to 'til":
-        print(cte[:int(input())])
 
 elif request == "strip":
     print(cte.strip())
@@ -68,8 +63,4 @@ elif request == "strip":
 else:
    print("finish your code")
 
-# This shit is supposed to work at least somehow. Otherwise, I'm going to use the try function
-# (The try function is a pain in the back)
-
-# Upd: I NEED to use the try function here to cath and deal occurring errors.
-# Mostly input errors: ValueError
+# In total, it should have 8 string operators
